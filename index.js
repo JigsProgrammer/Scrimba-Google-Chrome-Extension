0bce152b89280ca8130c9acdb5540c90dfd01803
John Lloyd Sunga <johnlloydsunga033@gmail.com>
2024-08-25 22:21:26 +0800

    Finished chrome extension lesson

--- index.js
+++ index.js
@@ -2,23 +2,51 @@ let myLeads = []
 const inputEl = document.getElementById("input-el")
 const inputBtn = document.getElementById("input-btn")
 const ulEl = document.getElementById("ul-el")
+const deleteBtn = document.getElementById("delete-btn")
+const leadsFromLocalStorage = JSON.parse(localStorage.getItem("myLeads"))
+const tabBtn = document.getElementById("tab-btn")
 
-inputBtn.addEventListener("click", function() {
-    myLeads.push(inputEl.value)
-    inputEl.value = ""
-    renderLeads()
+if (leadsFromLocalStorage) {
+    myLeads = leadsFromLocalStorage
+    render(myLeads)
+}
+
+
+/*This function, when clicked, queries chrome for a specific active tab in a current window. The function then calls the parameter tabs wherein the current tab url will be pushed into the myLeads array. Line 18 specifies that it should be a url by using .url. Line 19 shows the basic saving of the url into the localStorage with a JSON.stringify which is necessary. Line 20 basically renders the saved url tabs in the display window.*/
+tabBtn.addEventListener("click", function() {
+    chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+        myLeads.push(tabs[0].url)
+        localStorage.setItem("myLeads", JSON.stringify(myLeads))
+        render(myLeads)
+    })
 })
 
-function renderLeads() {
+/*This function with a parameter leads, take an argument stored in the parameter leads and then lists down the url typed down in the input element. Using a for loop, listing down becomes easier while using template literal to simplify the string display*/
+function render(leads) {
     let listItems = ""
-    for (let i = 0; i < myLeads.length; i++) {
+    for (let i = 0; i < leads.length; i++) {
         listItems += `
             <li>
-                <a target='_blank' href='${myLeads[i]}'>
-                    ${myLeads[i]}
+                <a target='_blank' href='${leads[i]}'>
+                    ${leads[i]}
                 </a>
             </li>
         `
     }
     ulEl.innerHTML = listItems  
 }
+
+deleteBtn.addEventListener("dblclick", function() {
+    localStorage.clear()
+    myLeads = []
+    render(myLeads)
+})
+
+inputBtn.addEventListener("click", function() {
+    myLeads.push(inputEl.value)
+    inputEl.value = ""
+    localStorage.setItem("myLeads", JSON.stringify(myLeads))
+    render(myLeads)
+})
+
+
